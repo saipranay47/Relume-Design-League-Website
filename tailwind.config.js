e3403bed2437232995c89306b685139b85a1e483
saipranay47 <saipranayadepu07@gmail.com>
2023-10-25 11:34:56 +0530

    animation typo

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,18 +7,18 @@ export default {
         "slide-in": {
           from: {
             transform:
-              "translate3d(0px, 50vh, 0px) scale3 rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+              "translate3d(0px, 50vh, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
             opacity: 0,
           },
           to: {
             transform:
-              "translate3d(0px, 0vh, 0px) scale3 rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
+              "translate3d(0px, 0vh, 0px) scale3d(1, 1, 1) rotateX(0deg) rotateY(0deg) rotateZ(0deg) skew(0deg, 0deg)",
             opacity: 1,
           },
         },
       },
       animation: {
-        "slide-in": "slide-in 3s ease forwards",
+        "slide-in": "slide-in 2s ease forwards",
       },
     },
   },
